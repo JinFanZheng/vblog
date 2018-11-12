1c2c7d2002164c45c82610f6f1fc6f07575a7484
Van <zheng_jinfan@126.com>
2018-11-12 12:34:29 +0800

    数据同步.

--- web/src/layouts/index.jsx
+++ web/src/layouts/index.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { connect } from 'dva';
+import Link from 'umi/link';
 import withRouter from 'umi/withRouter';
 import { Layout, Menu, Icon } from 'antd';
 import styles from './index.less';
@@ -27,12 +28,16 @@ class BasicLayout extends Component {
           <div className={styles.logo} />
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
             <Menu.Item key="1">
-              <Icon type="user" />
-              <span>nav 1</span>
+              <Link to="/login">
+                <Icon type="user" />
+                <span>nav 1</span>
+              </Link>
             </Menu.Item>
             <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span>nav 2</span>
+              <Link to="/">
+                <Icon type="video-camera" />
+                <span>nav 2</span>
+              </Link>
             </Menu.Item>
             <Menu.Item key="3">
               <Icon type="upload" />
